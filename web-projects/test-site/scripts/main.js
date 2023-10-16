c26a62a3c0e748ccf29c5557d3e140c19eb7f58a
saitomooo <tamaapples@gmail.com>
2023-10-16 12:15:23 +0900

    遷移画像の追加とmain.jsの更新

---
+++ web-projects/test-site/scripts/main.js
@@ -0,0 +1,33 @@
+const myHeading = document.querySelector("h1");
+myHeading.textContent = "Hello world!";
+
+const myImage = document.querySelector("img");
+
+myImage.onclick = () => {
+  const mySrc = myImage.getAttribute("src");
+  if (mySrc === "images/firefox-icon.png") {
+    myImage.setAttribute("src", "images/fire-fox2.jpg");
+  } else {
+    myImage.setAttribute("src", "images/fire-fox.png");
+  }
+};
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+function setUserName() {
+    const myName = prompt("あなたの名前を入力してください。");
+    if (!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem("name", myName);
+      myHeading.textContent = `${myName} さん、Mozilla はかっこいいよ。`;
+    }
+}
+  if (!localStorage.getItem("name")) {
+    setUserName();
+  } else {
+    const storedName = localStorage.getItem("name");
+    myHeading.textContent = `Mozilla はかっこいいよ、${storedName}`;
+  }
+  myButton.onclick = () => {
+    setUserName();
+  };
